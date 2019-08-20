b3083a1c0079bafa39a5aa39144b232bd81e3354
Enrique <edelvallep@gmail.com>
2019-08-20 06:39:23 -0500

    busqueda y eliminacion"

--- server/server.js
+++ server/server.js
@@ -1,48 +1,27 @@
-require('./config/config')
+require('./config/config');
+
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es requerido'
-        })
-
-    } else {
-        res.json({ persona: body })
+mongoose.connect('mongodb://localhost:27017/cafe', (err, resp) => {
+    if (err) {
+        throw err;
     }
 
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
+    console.log('Base de datos online');
 });
 
 app.listen(process.env.PORT, () => {
